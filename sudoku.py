10630904e3edea2ef71e10f79f39491495d50deb
Rocket Monkey <136569087+rocket-monkey-fun@users.noreply.github.com>
2023-09-11 16:39:15 +0200

    Add files via upload

--- sudoku.py
+++ sudoku.py
@@ -194,42 +194,48 @@ def callback_change_funny_text_2():
 
 def callback_set_to_number(sender, app_data):
     sound = get_value_sound()
-    dpg.configure_item(sender, texture_tag = app_data)
     row_column = "".join(number for number in sender if number.isdecimal())
     row = int(row_column[0])
     column = int(row_column[1])
-    new_number = int("".join(number for number in app_data if number.isdecimal()))
+    new_number_image_type = "".join(number for number in app_data if number.isdecimal())
+    new_number = int(new_number_image_type[0])
     old_number = sudoku[row - 1][column - 1]
 
-    if old_number != new_number:
-        print ("fudge")
-        print(f"button_{row}_{column}")
-        dpg.configure_item(f"button_{row}_{column}", texture_tag = f"image_{new_number}_c")
-        number_of_mistakes.append(0)
-        dpg.set_value("mistakes", len(number_of_mistakes))
-        if sound == "On":
-            incorrect.play()
-        time.sleep(1.5)
-        dpg.configure_item(f"button_{row}_{column}", texture_tag = f"image_0_a")
-        
-    if old_number == new_number:
-        position = (((row - 1) * 9) + column) - 1
-        easy_mode.remove(position)
-        if sound == "On":
-            correct.play()
-
-        if len(easy_mode) == 0:
-            print("win")
-            dpg.configure_item("finish_popup", show = True)
-            elapsed_time = dpg.get_value("timer")
-            dpg.set_value("stat_time", f"Your time is: {elapsed_time}")
-            dpg.set_value("stat_mistakes", f"Number of mistakes: {len(number_of_mistakes)}")
-            timer_on.pop()
-            timer_on.append(False)
+    if app_data == f"image_{new_number}2":
+        if old_number != new_number:
+            print ("fudge")
+            print(f"button_{row}_{column}")
+            print(new_number)
+            dpg.configure_item(f"button_{row}_{column}", texture_tag = f"image_{new_number}3")
+            number_of_mistakes.append(0)
+            dpg.set_value("mistakes", len(number_of_mistakes))
             if sound == "On":
-                win_game.play()
+                incorrect.play()
+            time.sleep(1.5)
+            dpg.configure_item(f"button_{row}_{column}", texture_tag = f"image_blank")
+            
+        if old_number == new_number:
+            dpg.configure_item(sender, texture_tag = app_data)
+            position = (((row - 1) * 9) + column) - 1
+            easy_mode.remove(position)
+            if sound == "On":
+                correct.play()
 
-    print(len(easy_mode))
+            if len(easy_mode) == 0:
+                print("win")
+                dpg.configure_item("finish_popup", show = True)
+                elapsed_time = dpg.get_value("timer")
+                dpg.set_value("stat_time", f"Your time is: {elapsed_time}")
+                dpg.set_value("stat_mistakes", f"Number of mistakes: {len(number_of_mistakes)}")
+                timer_on.pop()
+                timer_on.append(False)
+                if sound == "On":
+                    win_game.play()
+        print(len(easy_mode))
+
+    if app_data == f"image_{new_number}4":
+        dpg.configure_item(sender, texture_tag = app_data)
+
 
 def callback_start_game():
     sound = get_value_sound()
@@ -276,9 +282,9 @@ def create_easy_board():
         for column in range(1, 10):
             number_value = 9 * (row - 1) + (column - 1)
             test.append(flat_sudoku[number_value])
-            dpg.configure_item(f"button_{row}_{column}", texture_tag = f"image_{flat_sudoku[number_value]}_a")
+            print(flat_sudoku[number_value])
             if flat_sudoku[number_value] > 0:
-                dpg.configure_item(f"button_{row}_{column}", payload_type = "no_change")
+                dpg.configure_item(f"button_{row}_{column}", texture_tag = f"image_{flat_sudoku[number_value]}1", payload_type = "no_change")
         print(test)
         test.clear()
 
@@ -286,97 +292,50 @@ def get_value_sound():
     sound = dpg.get_value("sound")
     return sound    
 
+def load_image_resource(image_number, image_type):
+    width, height, channels, data = dpg.load_image(f"resources/image_{image_number}{image_type}.png")
+    return width, height, channels, data
+
+def add_static_texture(image_number, image_type):
+    dpg.add_static_texture(width = image_width[image_number - 1][image_type - 1], height = image_height[image_number - 1][image_type - 1], default_value = image_data[image_number - 1][image_type - 1], tag = f"image_{image_number}{image_type}")
+
 thread1 = threading.Thread(target = callback_new_game_generate)
 thread2 = threading.Thread(target = callback_change_funny_text)
 thread3 = threading.Thread(target = callback_change_funny_text_2)
 thread4 = threading.Thread(target = callback_start_timer)
 
-
+image_width = []
+image_height = []
+image_channels = []
+image_data = []
 
 ##############################################################################################################################################################################################
 
 dpg.create_context()
 
-width_0_a, height_0_a, channels_0_a, data_0_a = dpg.load_image("resources/image_0_a.png")
+for image_number in range(1, 10):
+    image_width.append([])
+    image_height.append([])
+    image_channels.append([])
+    image_data.append([])
+    for image_type in range(1, 5):
+        width, height, channels, data = load_image_resource(image_number, image_type)
+        image_width[image_number - 1].append(width)
+        image_height[image_number - 1].append(height)
+        image_channels[image_number - 1].append(channels)
+        image_data[image_number - 1].append(data)
 
-width_1_a, height_1_a, channels_1_a, data_1_a = dpg.load_image("resources/image_1_a.png")
-width_1_b, height_1_b, channels_1_b, data_1_b = dpg.load_image("resources/image_1_b.png")
-width_1_c, height_1_c, channels_1_c, data_1_c = dpg.load_image("resources/image_1_c.png")
-
-width_2_a, height_2_a, channels_2_a, data_2_a = dpg.load_image("resources/image_2_a.png")
-width_2_b, height_2_b, channels_2_b, data_2_b = dpg.load_image("resources/image_2_b.png")
-width_2_c, height_2_c, channels_2_c, data_2_c = dpg.load_image("resources/image_2_c.png")
-
-width_3_a, height_3_a, channels_3_a, data_3_a = dpg.load_image("resources/image_3_a.png")
-width_3_b, height_3_b, channels_3_b, data_3_b = dpg.load_image("resources/image_3_b.png")
-width_3_c, height_3_c, channels_3_c, data_3_c = dpg.load_image("resources/image_3_c.png")
-
-width_4_a, height_4_a, channels_4_a, data_4_a = dpg.load_image("resources/image_4_a.png")
-width_4_b, height_4_b, channels_4_b, data_4_b = dpg.load_image("resources/image_4_b.png")
-width_4_c, height_4_c, channels_4_c, data_4_c = dpg.load_image("resources/image_4_c.png")
-
-width_5_a, height_5_a, channels_5_a, data_5_a = dpg.load_image("resources/image_5_a.png")
-width_5_b, height_5_b, channels_5_b, data_5_b = dpg.load_image("resources/image_5_b.png")
-width_5_c, height_5_c, channels_5_c, data_5_c = dpg.load_image("resources/image_5_c.png")
-
-width_6_a, height_6_a, channels_6_a, data_6_a = dpg.load_image("resources/image_6_a.png")
-width_6_b, height_6_b, channels_6_b, data_6_b = dpg.load_image("resources/image_6_b.png")
-width_6_c, height_6_c, channels_6_c, data_6_c = dpg.load_image("resources/image_6_c.png")
-
-width_7_a, height_7_a, channels_7_a, data_7_a = dpg.load_image("resources/image_7_a.png")
-width_7_b, height_7_b, channels_7_b, data_7_b = dpg.load_image("resources/image_7_b.png")
-width_7_c, height_7_c, channels_7_c, data_7_c = dpg.load_image("resources/image_7_c.png")
-
-width_8_a, height_8_a, channels_8_a, data_8_a = dpg.load_image("resources/image_8_a.png")
-width_8_b, height_8_b, channels_8_b, data_8_b = dpg.load_image("resources/image_8_b.png")
-width_8_c, height_8_c, channels_8_c, data_8_c = dpg.load_image("resources/image_8_c.png")
-
-width_9_a, height_9_a, channels_9_a, data_9_a = dpg.load_image("resources/image_9_a.png")
-width_9_b, height_9_b, channels_9_b, data_9_b = dpg.load_image("resources/image_9_b.png")
-width_9_c, height_9_c, channels_9_c, data_9_c = dpg.load_image("resources/image_9_c.png")
-
-width_99, height_99, channels_99, data_99 = dpg.load_image("resources/image_99.png")
+width_blank, height_blank, channels_blank, data_blank = dpg.load_image("resources/image_blank.png")
+width_reset, height_reset, channels_reset, data_reset = dpg.load_image("resources/image_reset.png")
 
 with dpg.texture_registry():
-    dpg.add_static_texture(width = width_0_a, height = height_0_a, default_value = data_0_a, tag = "image_0_a")
-    
-    dpg.add_static_texture(width = width_1_a, height = height_1_a, default_value = data_1_a, tag = "image_1_a")
-    dpg.add_static_texture(width = width_1_b, height = height_1_b, default_value = data_1_b, tag = "image_1_b")
-    dpg.add_static_texture(width = width_1_c, height = height_1_c, default_value = data_1_c, tag = "image_1_c")
 
-    dpg.add_static_texture(width = width_2_a, height = height_2_a, default_value = data_2_a, tag = "image_2_a")
-    dpg.add_static_texture(width = width_2_b, height = height_2_b, default_value = data_2_b, tag = "image_2_b")
-    dpg.add_static_texture(width = width_2_c, height = height_2_c, default_value = data_2_c, tag = "image_2_c")
+    for image_number in range(1, 10):
+        for image_type in range(1, 5):
+            add_static_texture(image_number, image_type)
 
-    dpg.add_static_texture(width = width_3_a, height = height_3_a, default_value = data_3_a, tag = "image_3_a")
-    dpg.add_static_texture(width = width_3_b, height = height_3_b, default_value = data_3_b, tag = "image_3_b")
-    dpg.add_static_texture(width = width_3_c, height = height_3_c, default_value = data_3_c, tag = "image_3_c")
-
-    dpg.add_static_texture(width = width_4_a, height = height_4_a, default_value = data_4_a, tag = "image_4_a")
-    dpg.add_static_texture(width = width_4_b, height = height_4_b, default_value = data_4_b, tag = "image_4_b")
-    dpg.add_static_texture(width = width_4_c, height = height_4_c, default_value = data_4_c, tag = "image_4_c")
-
-    dpg.add_static_texture(width = width_5_a, height = height_5_a, default_value = data_5_a, tag = "image_5_a")
-    dpg.add_static_texture(width = width_5_b, height = height_5_b, default_value = data_5_b, tag = "image_5_b")
-    dpg.add_static_texture(width = width_5_c, height = height_5_c, default_value = data_5_c, tag = "image_5_c")
-
-    dpg.add_static_texture(width = width_6_a, height = height_6_a, default_value = data_6_a, tag = "image_6_a")
-    dpg.add_static_texture(width = width_6_b, height = height_6_b, default_value = data_6_b, tag = "image_6_b")
-    dpg.add_static_texture(width = width_6_c, height = height_6_c, default_value = data_6_c, tag = "image_6_c")
-
-    dpg.add_static_texture(width = width_7_a, height = height_7_a, default_value = data_7_a, tag = "image_7_a")
-    dpg.add_static_texture(width = width_7_b, height = height_7_b, default_value = data_7_b, tag = "image_7_b")
-    dpg.add_static_texture(width = width_7_c, height = height_7_c, default_value = data_7_c, tag = "image_7_c")
-
-    dpg.add_static_texture(width = width_8_a, height = height_8_a, default_value = data_8_a, tag = "image_8_a")
-    dpg.add_static_texture(width = width_8_b, height = height_8_b, default_value = data_8_b, tag = "image_8_b")
-    dpg.add_static_texture(width = width_8_c, height = height_8_c, default_value = data_8_c, tag = "image_8_c")
-
-    dpg.add_static_texture(width = width_9_a, height = height_9_a, default_value = data_9_a, tag = "image_9_a")
-    dpg.add_static_texture(width = width_9_b, height = height_9_b, default_value = data_9_b, tag = "image_9_b")
-    dpg.add_static_texture(width = width_9_c, height = height_9_c, default_value = data_9_c, tag = "image_9_c")
-
-    dpg.add_static_texture(width = width_99, height = height_99, default_value = data_99, tag = "image_99")
+    dpg.add_static_texture(width = width_blank, height = height_blank, default_value = data_blank, tag = "image_blank")
+    dpg.add_static_texture(width = width_reset, height = height_reset, default_value = data_reset, tag = "image_reset")
 
 dpg.create_viewport(title = 'Sudoku', width = 600, height = 600, small_icon = "resources/icon.ico", large_icon = "resources/icon.ico", resizable = False)
 
@@ -411,7 +370,6 @@ with dpg.window(label = "Welcome screen", pos = (100, 100), tag = "welcome_scree
     dpg.add_text("Like really, really slow...", show = False, tag = "funny_3")
     dpg.add_text("Like really, really, really slow...", show = False, tag = "funny_4")
 
-
     dpg.add_button(label = "     Start game     ", callback = callback_start_game, show = False, tag = "start_game")
     dpg.bind_item_theme(dpg.last_item(), "button_theme")
 
@@ -419,15 +377,19 @@ with dpg.window(label = "Game screen", pos = (100, 100), show = False, tag = "ga
     for row in range(1, 10):
         with dpg.group(horizontal = True):
             for column in range(1, 10):
-                dpg.add_image_button("image_0_a", width = 30, height = 30, drop_callback = callback_set_to_number, payload_type = "strings", tag = f"button_{row}_{column}")
+                dpg.add_image_button("image_blank", width = 30, height = 30, drop_callback = callback_set_to_number, payload_type = "strings", tag = f"button_{row}_{column}")
  
                 if row > 0 and column == 9:
-                    dpg.add_image_button(f"image_{row}_b", width = 30, height = 30, indent = 450)
-                    with dpg.drag_payload(parent = dpg.last_item(), drag_data = f"image_{row}_b", payload_type = "strings"):
+                    dpg.add_image_button(f"image_{row}2", width = 30, height = 30, indent = 450)
+                    with dpg.drag_payload(parent = dpg.last_item(), drag_data = f"image_{row}2", payload_type = "strings"):
                         dpg.add_text(f"{row}")
 
-    dpg.add_image_button("image_99", width = 30, height = 30, indent = 450, show = False)
-    with dpg.drag_payload(parent = dpg.last_item(), drag_data = "image_0_a", payload_type = "strings"):
+                    dpg.add_image_button(f"image_{row}4", width = 30, height = 30, indent = 500)
+                    with dpg.drag_payload(parent = dpg.last_item(), drag_data = f"image_{row}4", payload_type = "strings"):
+                        dpg.add_text(f"{row}")
+
+    dpg.add_image_button("image_reset", width = 30, height = 30, indent = 450, show = False)
+    with dpg.drag_payload(parent = dpg.last_item(), drag_data = "image_blank", payload_type = "strings"):
         dpg.add_text("reset")
 
     with dpg.draw_layer():
--- text.py
+++ text.py
@@ -38,4 +38,4 @@ gamemode_continuous = "Check continuously: In this mode each move is checked aft
 
 gamemode_finish = "Check on finish: In this mode the result is shown after the last move is done."
 
-instructions_gamemode_continuous = "- Drag and drop the desired number from the right hand side to the target cell.\n- If the value is correct, you can continue.\n- If it turns red, you have entered an incorrect value.\n- Have fun!"
+instructions_gamemode_continuous = "- The green numbers serve as pen marks and are checked.\n- If the value is correct, you can continue.\n- If it turns red, you have entered an incorrect value.\n- The brown numbers serve as pencil marks and are not checked.\n- Have fun!"
